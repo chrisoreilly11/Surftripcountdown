33b353c39e570a62c7305b4b0ea79c8cfa336fe2
chrisoreilly11 <chrisoreilly11@gmail.com>
2019-08-02 12:16:46 +1000

    Update main.js
    Commands and functions to get and display data

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-http://magicseaweed.com/api/0491dd8a14784887e8e150d44fea9953/forecast/?spot_id=867&fields=timestamp,swell.*,wind.*,condition.temperature,charts.*
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+    targetUrl = 'http://magicseaweed.com/api/0491dd8a14784887e8e150d44fea9953/forecast/?spot_id=867&fields=timestamp,localTimestamp,swell.*,wind.*,condition.temperature'
+    fetch(proxyUrl + targetUrl)
+    .then(blob => blob.json())
+    .then(data => {
+
+    	$.each(data, function(index, value) {
+    		console.log(value);
+
+
+
+  	       var temperature = value.condition.temperature;
+  	       console.log(temperature);
+  	       var swell = value.swell.absMaxBreakingHeight; 
+
+  	       // these commands will put data into browser
+  	      // $('.temperature').text(temperature);
+  	      // $('.swell').text(swell);
+
+  	       // loops back through data to append all of it
+  	       $('.output').append('<h1 class="temperature">' + temperature + '</h1><h2 class="swell">' + swell + '</h2>');
+
+    	})
+
+    })
